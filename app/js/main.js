c303974b5a5329a43dbcfe315074d5537dca4955
pika-1337 <pika_sergiu@ukr.net>
2022-05-03 21:15:27 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
 $(function(){
-  
+
+
+  $('.shop-content__filter-btn').on('click', function (){
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function(){
+    $('.products__items-info').addClass('products__items--list')
+  });
+
+    $('.button-grid').on('click', function(){
+    $('.products__items-info').removeClass('products__items--list')
+  });
+
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix:"$",
+    onStart: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+        $('.filter-price__from').text(data.from);
+        $('.filter-price__to').text(data.to);
+    }
+  });
+
 
   $('.top__slider-inner').slick({
     dots:true,
@@ -11,6 +39,27 @@ $(function(){
     
   });
 
+  $(".star").rateYo({
+    
+    starWidth: "11px",
+    starHeight: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+
+  });
+
+
+    $(".products-star").rateYo({
+    
+    starWidth: "11px",
+    starHeight: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "12px"
+
+  });
 });
 $(function(){
   var mixer = mixitup('.products__items');
--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,9 @@ const browserSync = require('browser-sync').create();
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-      'app/js/main.js'
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'app/js/main.js',
     ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
