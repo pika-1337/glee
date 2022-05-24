30c1d4ebe0652b0539aa3a21f1ceba29d9cfbbc7
pika-1337 <pika_sergiu@ukr.net>
2022-05-24 14:03:06 +0300

    added module html and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const {src, dest, watch, parallel, series} = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
-const browserSync = require('browser-sync').create();
+const scss =            require('gulp-sass')(require('sass'));
+const concat =          require('gulp-concat');
+const autoprefixer =    require('gulp-autoprefixer');
+const uglify =          require('gulp-uglify');
+const imagemin =        require('gulp-imagemin');
+const rename =        require('gulp-rename');
+const nunjucksRender =  require('gulp-nunjucks-render');
+const del =             require('del');
+const browserSync =     require('browser-sync').create();
 
 
     function browsersync() {
@@ -17,18 +19,26 @@ const browserSync = require('browser-sync').create();
     });
     }
 
+    function nunjucks() {
+      return src('app/*.njk')
+        .pipe(nunjucksRender())
+        .pipe(dest('app'))
+        .pipe(browserSync.stream())
+    }
 
   function styles()  {
-    return src('app/scss/style.scss')
+    return src('app/**/*.scss')
     .pipe(scss({outputStyle:'compressed'}))
-    .pipe(concat('style.min.css'))
+    // .pipe(concat())
+    .pipe(rename({
+      suffix : '.min'
+    }))
     .pipe(autoprefixer({
       overrideBrowserslist:['last 10 versions'],grid:true
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
   }
-
 
   function scripts() {
     return src([
@@ -78,18 +88,18 @@ const browserSync = require('browser-sync').create();
 
   function watching() {
     watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], styles);
+    watch(['app/*.njk'], nunjucks);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
     watch(['app/**/*.html']).on('change', browserSync.reload);
   }
 
-
-  exports.browsersync = browsersync;
   exports.styles = styles;
   exports.scripts = scripts;
-  exports.images = images;
+  exports.browsersync = browsersync;
   exports.watching = watching;
+  exports.images = images;
+  exports.nunjucks = nunjucks;
   exports.cleanDist = cleanDist;
   exports.build = series(cleanDist,images, build);
 
-
-exports.default = parallel(styles,scripts, browsersync,watching); 
+exports.default = parallel(nunjucks,styles,scripts, browsersync,watching); 
